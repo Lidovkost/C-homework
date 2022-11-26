a31037c84d9ac29185a4d9b35c761f1745860766
«Lidovskiy_Konstantin» <lidovkost@gmail.com>
2022-11-26 20:47:31 +0300

    задание 5 небольшие изменения в методе

--- homework5/Program.cs
+++ homework5/Program.cs
@@ -3,13 +3,13 @@
 int number = 0;
 int prompt(string message)
 {
-    Console.Write($"Введите число в диапазоне 99<|{message}|<1000 ");
+    Console.Write(message);
     int answer = Convert.ToInt32(Console.ReadLine());
     return answer;
 }
 while (number > 999 | number < 100)
 {
-    number = prompt("трехзначное число");
+    number = prompt("Введите трехзначное число ");
     number = Math.Abs(number);
 }
 
