04bba63bf9eb06eebd4bdd5e03924a816a5984b8
«Lidovskiy_Konstantin» <lidovkost@gmail.com>
2022-12-06 22:40:21 +0300

    Homework15 Homework16

---
+++ Homework15/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] createAraay()
+{
+    Console.WriteLine("введите количество элементов массива");
+    int numberOfElements = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[numberOfElements];
+    return array;
+}
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+void rndArrayElements(int[] array)
+{
+    Random rnd = new Random();
+    Console.WriteLine("Введите минимальный порог случайных значений");
+    int minrnd = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальный порог случайных значений");
+    int maxrnd = Convert.ToInt32(Console.ReadLine()) + 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minrnd, maxrnd);
+    }
+
+}
+int[] testArray = createAraay();
+rndArrayElements(testArray);
+int evenNumbers(int[] array)
+{
+    int evenNumbers = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) evenNumbers = evenNumbers + 1;
+    }
+    return evenNumbers;
+}
+int numberOfEvenNumbers = evenNumbers(testArray);
+printArray(testArray);
+Console.WriteLine();
+Console.WriteLine(numberOfEvenNumbers);
+
+
+// без метода
+// int arraylength = testArray.Length;
+// int evenNumbers = 0;
+//     for(int i = 0; i < testArray.Length; i++ )
+//     {
+//         if(testArray[i]%2==0) evenNumbers = evenNumbers +1;
+//     }    
+// printArray(testArray);
+// Console.WriteLine();
+// Console.WriteLine(evenNumbers);
---
+++ Homework16/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+int[] createAraay()
+{
+    Console.WriteLine("введите количество элементов массива");
+    int numberOfElements = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[numberOfElements];
+    return array;
+}
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+void rndArrayElements(int[] array)
+{
+    Random rnd = new Random();
+    Console.WriteLine("Введите минимальный порог случайных значений");
+    int minrnd = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальный порог случайных значений");
+    int maxrnd = Convert.ToInt32(Console.ReadLine()) + 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minrnd, maxrnd);
+    }
+
+}
+int[] testArray = createAraay();
+rndArrayElements(testArray);
+printArray(testArray);
+int SumOfelEmentsOfOddPositions(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+int sum = SumOfelEmentsOfOddPositions(testArray);
+Console.WriteLine();
+Console.WriteLine(sum);
